a07046a07fdcaa0bf3b62f797e836dce4f9f9e6c
Mouna Khila <khilamouna.Simplonenda@gmail.com>
2021-04-14 16:10:13 +0200

    ajout js

--- js/script.js
+++ js/script.js
@@ -13,15 +13,16 @@ updateCartTotal()
 
 
 
-function updateCartTotal(){
-let cartItemContainer=document.getElementsByClassName('cart-items')[0]
-let cartRows= cartItemContainer.getElementsByClassName('cart-row')
-for(let i=0; i<cartRows.length;i++){
-let cartRow= cartRows[i]
-let priceElement=cartRow.getElementsByClassName('cart-price')[0]
-let quantityElement=cartRow.getElementsByClassName('cart-quantity-input')[0]
-console.log(priceElement,quantityElement)
-let price = priceElement.innerText
-console.log(cart-price)
-   }
-}
+
+      //function updateCartTotal(){
+        //let cartItemContainer=document.getElementsByClassName('cart-items')[0]
+        //let cartRows= cartItemContainer.getElementsByClassName('cart-row')
+        //for(let i=0; i<cartRows.length;i++){
+        //let cartRow= cartRows[i]
+        //let priceElement=cartRow.getElementsByClassName('cart-price')[0]
+        //let quantityElement=cartRow.getElementsByClassName('cart-quantity-input')[0]
+        //console.log(priceElement,quantityElement)
+        //let price = priceElement.innerText
+        //console.log(cart-price)
+          // }
+        //}
