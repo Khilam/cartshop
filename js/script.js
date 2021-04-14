27a5b1985985b903f4de64c4ba659165f01abce0
Mouna Khila <khilamouna.Simplonenda@gmail.com>
2021-04-14 08:30:04 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+let removeCartItemButtons= document.getElementsByClassName('btn-remove')
+console.log(removeCartItemButtons)
+for (let i=0; i<removeCartItemButtons.length;i++){
+    let button=removeCartItemButtons[i]
+   button.addEventListener('click', function(event){
+     
+       let buttonClicked= event.target
+buttonClicked.parentElement.parentElement.remove()
+updateCartTotal()
+      })
+      }
+
+
+
+function updateCartTotal(){
+let cartItemContainer=document.getElementsByClassName('cart-items')[0]
+let cartRows= cartItemContainer.getElementsByClassName('cart-row')
+for(let i=0; i<cartRows.length;i++){
+let cartRow= cartRows[i]
+let priceElement=cartRow.getElementsByClassName('cart-price')[0]
+let quantityElement=cartRow.getElementsByClassName('cart-quantity-input')[0]
+console.log(priceElement,quantityElement)
+let price = priceElement.innerHTML
+console.log(cart-price)
+   }
+}
