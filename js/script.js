b690200b535d0f52792b8fbe8228633b76153506
Mouna Khila <khilamouna.Simplonenda@gmail.com>
2021-04-15 00:51:24 +0200

    firt fonctionality

--- js/script.js
+++ js/script.js
@@ -1,15 +1,85 @@
+let carts=document.querySelectorAll('.add-cart');
+let products = [
+    {
+        name:'Guess',
+        price:500,
+        tag:'femme',
+        inCart:0
+    },
+    {
+        name:'Guess',
+        price:450,
+        tag:'homme',
+        inCart:0
+    },
+    {
+        name:'Festina',
+        price:250,
+        tag:'homme',
+        inCart:0
+    },
+    {
+        name:'Festina',
+        price:500,
+        tag:'femme',
+        inCart:0
+    },
+    {
+        name:'Montre Céramique Noire Diamants Femme',
+        price:800,
+        tag:'femme',
+        inCart:0
+    }
+];
+for (let i=0; i< carts.length; i++){
+    carts[i].addEventListener('click', ()=>{
+        cartNumbers();
+    })
+}
+function onLoadCartNumbers(){
+    let productNumbers=localStorage.getItem('cartNumbers');
+    if (productNumbers){
+        document.querySelector('.badge').textContent=productNumbers;
 
-let removeCartItemButtons= document.getElementsByClassName('btn-remove')
-console.log(removeCartItemButtons)
-for (let i=0; i<removeCartItemButtons.length;i++){
-    let button=removeCartItemButtons[i]
-   button.addEventListener('click', function(event){
-     
-       let buttonClicked= event.target
-buttonClicked.parentElement.parentElement.remove()
-updateCartTotal()
-      })
+    }
+
+}
+  function cartNumbers(){
+      let productNumbers=localStorage.getItem('cartNumbers');
+      console.log(productNumbers)
+      productNumbers=parseInt(productNumbers)
+   
+
+      if (productNumbers){
+        localStorage.setItem('cartNumbers',productNumbers +1);  
+        document.querySelector('badge').textContent=productNumbers +1 
+      }else{
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.badge').textContent=1
       }
+
+  }   
+     
+  onLoadCartNumbers();
+
+
+
+
+
+
+
+
+//let removeCartItemButtons= document.getElementsByClassName('btn-remove')
+//console.log(removeCartItemButtons)
+//for (let i=0; i<removeCartItemButtons.length;i++){
+   // let button=removeCartItemButtons[i]
+  // button.addEventListener('click', function(event){
+     
+     //  let buttonClicked= event.target
+//buttonClicked.parentElement.parentElement.remove()
+//updateCartTotal()
+      //})
+      //}
 
 
 
